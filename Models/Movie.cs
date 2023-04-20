16dccc37e7d3c8dfe51864e0d562f9b2f9fc1095
wendellmoraisz <wendellmorais3319@gmail.com>
2023-04-20 20:15:22 -0300

    feat: Movie model validations

--- Models/Movie.cs
+++ Models/Movie.cs
@@ -1,5 +1,6 @@
 using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -7,7 +8,12 @@ namespace MoviesApi.Models;
 
 public class Movie
 {
+    [Required(ErrorMessage = "The Movie Title is required")]
     public string Title { get; set; }
+    [Required(ErrorMessage = "The Movie Title is required")]
+    [MaxLength(50, ErrorMessage = "The Movie Genre length is too longer")]
     public string Genre { get; set; }
+    [Required]
+    [Range(70, 600, ErrorMessage = "The Duration must be between 70 and 600 minutes")]
     public int Duration { get; set; }
 }
