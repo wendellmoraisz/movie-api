82f9d4c0932e55c9b77deb8c117b7f41b5eeb6c9
wendellmoraisz <wendellmorais3319@gmail.com>
2023-04-21 09:56:32 -0300

    feat: inject dependencies in movie controller

--- Controllers/MovieController.cs
+++ Controllers/MovieController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using MoviesApi.Data;
 using MoviesApi.Models;
 
 namespace MoviesApi.Controllers;
@@ -11,28 +12,31 @@ namespace MoviesApi.Controllers;
 [Route("[controller]")]
 public class MovieController : ControllerBase
 {
+    private MovieContext _context;
 
-    private static List<Movie> movies = new List<Movie>();
-    private static int id = 0;
+    public MovieController(MovieContext context)
+    {
+        _context = context;
+    }
 
     [HttpPost]
     public IActionResult AddMovie([FromBody] Movie movie)
     {
-        movie.Id = id++;
-        movies.Add(movie);
+        _context.Movies.Add(movie);
+        _context.SaveChanges();
         return CreatedAtAction(nameof(GetMovieById), new { id = movie.Id }, movie);
     }
 
     [HttpGet]
     public IEnumerable<Movie> GetMovies([FromQuery] int skip = 0, [FromQuery] int take = 50)
     {
-        return movies.Skip(skip).Take(take);
+        return _context.Movies.Skip(skip).Take(take);
     }
 
     [HttpGet("{id}")]
     public IActionResult GetMovieById(int id)
     {
-        var movie = movies.FirstOrDefault(movie => movie.Id == id);
+        var movie = _context.Movies.FirstOrDefault(movie => movie.Id == id);
         if (movie == null) return NotFound();
         return Ok(movie);
     }
